f3cf855ae237083bcc4272c7c1020c17713a8159
Felprogrammer <evan.mcintire@yahoo.com>
2013-11-03 01:31:39 -0500

    Main Bulk of /openinv
    Added the main bulk of the /openinv command
    Creates a new blank Inventory and copies the items over, as well as the
    armor.

--- src/com/mcintire/evan/CommandOpenInv.java
+++ src/com/mcintire/evan/CommandOpenInv.java
@@ -1,9 +1,13 @@
 package com.mcintire.evan;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class CommandOpenInv implements CommandExecutor, Listener {
 	private OpenInv	plugin;
@@ -14,7 +18,22 @@ public class CommandOpenInv implements CommandExecutor, Listener {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+		if (cmd.getName().equalsIgnoreCase("openinv")) {
+			Player p = (Player) sender;
+			
+			Inventory i = Bukkit.createInventory(p, 45);
+			int slot = 0;
+			for (ItemStack item : p.getInventory().getContents()) {
+				i.setItem(slot, item);
+				slot++;
+			}
+			slot = 36;
+			for (ItemStack armorPiece : p.getInventory().getArmorContents()) {
+				i.setItem(slot, armorPiece);
+				slot++;
+			}
+			p.openInventory(i);
+		}
 		return false;
 	}
 }
